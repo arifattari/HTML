1c6ca2010810bd16b186d203be0210ddfc255668
arifattari <attariarif901>
2023-08-16 22:29:36 +0500

    java script

---
+++ Java Script/Class 1.js
@@ -0,0 +1,32 @@
+/*var ko name dete hein
+wo koi reserve keyword nhi ho sakta
+wo in {0 } se start nhi ho sakta*/
+
+ /*var student = 'Error!Plese enter a valid password';  
+ alert (student);
+
+
+ var student1 = 'Walcome to JS Land.. \n Happy Coding'
+alert (student1);
+
+var student2 = 'Hello... I can run JS through my web browser'
+console.log (student2);*/
+
+/*var jug = 'water'
+alert (jug);*/
+
+
+// var studentName = 'Arif';
+var studentid =  400;
+var studentincome = studentid * 10;
+var studentTex = 2 + studentidincome  / 100 * 2;
+
+alert (studentincome);
+ console.log (studentTex);
+// alert (studentName + ","+ studentid);
+// console.log (studentincome);
+
+
+
+
+
