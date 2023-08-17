d0d4695eb6ff2c2c55396fb0d4ea13eb0e0926a7
arifattari <attariarif901>
2023-08-17 10:56:50 +0500

    work complete

--- Java Script/Class 1.js
+++ Java Script/Class 1.js
@@ -10,23 +10,69 @@ wo in {0 } se start nhi ho sakta*/
 alert (student1);
 
 var student2 = 'Hello... I can run JS through my web browser'
-console.log (student2);*/
+console.log (student2);
 
-/*var jug = 'water'
-alert (jug);*/
+var jug = 'water'
+alert (jug);
 
 
-// var studentName = 'Arif';
+var studentName = 'Arif';
 var studentid =  400;
 var studentincome = studentid * 10;
-var studentTex = 2 + studentidincome  / 100 * 2;
+var studentTex = (2 + studentincome ) / 100 * 2;*/
 
-alert (studentincome);
- console.log (studentTex);
-// alert (studentName + ","+ studentid);
+
+var visitCount= +localStorage.getItem("visitCount");
+console.log (visitCount);
+ visitCount= ++visitCount;
+
+localStorage.setItem("visitCount", visitCount);
+alert ("You have visited this page" + visitCount + "times" );
+
+
+/*alert (studentincome);
+ console.log (studentTex);*/
+//  alert (studentName + ","+ studentid);
 // console.log (studentincome);
 
+// var myName = 'ARIF';
+// console.log(myName);
+
+var email = "attariarif@gmail.com";
+alert ("my email is " +email);
+
+// 1. pre increament
+var counter = 10;
+var newValue = ++counter
+console.log (counter);
+//  console.log (newValue);
+
+// post increment
+var counter = 10;
+var newValue = counter++
+console.log (counter);
+// console.log (newValue);
+
+/*var counter = 10;
+counter = counter + 1 ;
+counter = counter + 1 ;
+counter ++;
+counter ++;
+console.log (counter);*/
+
+// concatenating
+
+var greet = "hello from class";
+var classNumber = 27;
+var newString = greet + classNumber;
+console.log (newString);
+
+var studenID = 15416;
+var studentName = 'arif';
+var isStudentAvailable = 'No';
+console.log ('student Id: ' + studenID + ''  + ' student name: ' + studentName + '' + ' isstudentavailable: ' + isStudentAvailable );
 
 
-
-
+// promt
+var name = prompt ('Tell me your name');
+console.log (name);
